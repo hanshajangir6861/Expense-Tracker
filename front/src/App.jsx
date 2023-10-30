cfa073f3d993f19a8015eb5c70cf49ed8867f27d
unknown <hanshajangir061@gmail.com>
2023-10-30 11:31:12 -0700

    first

---
+++ front/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './App.css'
+
+
+function App() {
+
+
+
+
+
+
+
+
+  return (
+    <div className='main'>
+      
+      <div className='header'>
+
+      <h4>header</h4>
+      <ul>
+        <li>Home</li>
+        <li>Expense</li>
+        <li>Budget</li>
+      </ul>
+      </div>
+      
+      <div className='button'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
